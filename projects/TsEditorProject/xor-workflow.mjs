d058b55b243e57b6cbf81a761984cee6cf51b042
throw-out <45587825+throw-out@users.noreply.github.com>
2023-01-11 16:46:20 +0800

    delete invalid code

--- projects/TsEditorProject/xor-workflow.mjs
+++ projects/TsEditorProject/xor-workflow.mjs
@@ -75,68 +75,6 @@ const commands = new class {
     }
 }
 
-/**
- * 
- * @param {minimist.ParsedArgs} options 
- */
-function copydir(options) {
-    program.requiredOption("--source <source>", "缺少必要的参数: 源文件路径");
-    program.requiredOption("--target <target>", "缺少必要的参数: 目标文件路径");
-    program.option("--filter <filter>", "指定文件后缀名", ".js|.mjs");
-    program.option("--recursion <recursion>", "递归文件夹", "true");
-    program.option("--append <append>", "追加文件名");
-
-    program.parse(process.argv);
-    //const options = program.opts();
-
-    let { source, target, filter, recursion, append = '' } = options;
-
-    /**@type {string[]} */
-    let filters = filter.split("|");
-    /**是否过滤文件
-     * @param {string} extname 
-     */
-    function isFilters(extname) {
-        if (filters && filters.length > 0 && !filters.includes(extname)) {
-            return false;
-        }
-        return true;
-    }
-    /**
-     * @param {string} source 
-     * @param {string} target 
-     * @param {boolean} recursion 
-     */
-    function copy(source, target, recursion) {
-        let stat = fs.statSync(source);
-        if (stat.isFile()) {
-            if (!isFilters(path.extname(source)))
-                return;
-            let dirpath = path.dirname(target);
-            if (!fs.existsSync(dirpath)) {
-                fs.mkdirSync(dirpath, { recursive: true });
-            }
-            fs.copyFileSync(source, `${target}${append}`);
-        } else if (stat.isDirectory()) {
-            for (let dirname of fs.readdirSync(source)) {
-                copy(
-                    path.join(source, dirname),
-                    path.join(target, dirname),
-                    recursion
-                );
-            }
-        }
-
-    }
-    let dirname = path.dirname(import.meta.url.replace("file:///", ""));
-    copy(
-        path.join(dirname, source),
-        path.join(dirname, target),
-        recursion !== "false"
-    );
-    console.log("complete.");
-}
-
 const options = minimist(process.argv.slice(2));
 const method = options["_"].find(m => m in commands);
 if (method) {
